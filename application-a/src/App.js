fc639be39d0e8175f9014363a6047db0096a18d8
sergiod <sergiod@us.ibm.com>
2023-06-29 14:22:24 -0400

    REACT 9th commit: Fixed URL to invoke another micro-service.

--- application-a/src/App.js
+++ application-a/src/App.js
@@ -5,15 +5,16 @@ import { configuration } from './config.js';
 export default function LogComponent() {
 
   const [counter, setCounter] = useState(0);
-    const intervalIDRef = React.useRef(null);
-const msg = configuration.seed;
+  const intervalIDRef = React.useRef(null);
+  const msg = configuration.seed;
+  const url = configuration.fetchUrl+"/message?content="+message;
 
   //increase counter
   const start_log = () => {
   intervalIDRef.current = setInterval(() => {
     const message = "Test " +  msg + " Message " + counter;
     console.log(message);
-    fetch(configuration.fetchUrl+"/message?content="+message)
+    fetch(url)
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }));
     setCounter(count => count + 1);
